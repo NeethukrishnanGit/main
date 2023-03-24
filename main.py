8b4b9dbdef1c53fda84f99b7dce51ca3e536c468
ANORA\neethukrishnan <neethukrishnan@anoralabs.com>
2023-03-24 10:29:23 +0530

    Added the menu driven program base for the project purpose

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+def default_function():
+    print("Hello! Welcome to Menu driven Program")
+
+
 if __name__ == "__main__":
-    print("menu driven program")
+
+    print("0. Exit")
+
+    print("1. Default Choice")
+
+    while True:
+
+        choice = int(input("Enter a choice"))
+
+        if not choice:
+            break
+
+        if choice == 1:
+            default_function()
+
+    print("Thanks! Come Again")
