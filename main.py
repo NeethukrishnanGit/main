b5df6693b435305fd14e3488ec2ddc37c0d2d26b
ANORA\ajays <ajays@anoralabs.com>
2023-03-24 11:21:39 +0530

    addition for menu driven program by ajay

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 def default_function():
     print("Hello! Welcome to Menu driven Program")
 
-
+def add_function(x,y):
+    sum=x+y
+    print("the addition of 2 operand is :" , sum)
 if __name__ == "__main__":
     print("0. Exit")
     print("1. Default Choice")
+
+    print("2. addition")
 
     while True:
 
@@ -15,5 +19,10 @@ if __name__ == "__main__":
 
         if choice == 1:
             default_function()
+
+        if choice == 2:
+            operand1 = int(input("enter the first operand:"))
+            operand2 = int(input("enter the second operand:"))
+            add_function(operand1,operand2)
 
     print("Thanks! Come Again")
