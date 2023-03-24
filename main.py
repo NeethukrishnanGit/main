d48a5568f7d6e78a82d301b625878c869735f7a4
Arini12# <arini@anorasolutions.com>
2023-03-24 15:17:39 +0530

    resolved conflict regarding the division part

--- main.py
+++ main.py
@@ -11,12 +11,21 @@ def sub_function(x, y):
     sub = x - y
     print("the subtraction of 2 operand is :", sub)
 
+
+def div_func(x, y):
+    div = x / y
+    print("division is:", div)
+
+
 if __name__ == "__main__":
+
     print("0. Exit")
+
     print("1. Default Choice")
 
     print("2. addition")
     print("3. subtraction")
+    print("6 . division")
 
     while True:
 
@@ -36,5 +45,9 @@ if __name__ == "__main__":
             operand1 = int(input("enter the first operand:"))
             operand2 = int(input("enter the second operand:"))
             sub_function(operand1, operand2)
+        if choice == 6:
+            num1 = int(input("enter the number: "))
+            num2 = int(input("enter the number: "))
+            div_func(num1, num2)
 
         print("Thanks! Come Again")
