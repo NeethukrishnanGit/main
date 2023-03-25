0fe883b65d732f96375c331e5b4a7c9d8a98d1ee
ANORA\ajays <ajays@anoralabs.com>
2023-03-25 13:40:43 +0530

    PROGRAM FOR FINAL ASSIGNMENT BY AJAY

--- assignment.py
+++ assignment.py
@@ -1,3 +1,13 @@
+# CODE FOR INPUT  STRING IS PALINDROM OR NOT
+
+input_string=input("enter the string: \n")
+reversed_string= input_string[::-1]
+if input_string == reversed_string:
+    print(f"the given string \"{input_string}\" is palindrom")
+else:
+    print(f"the given string \"{input_string}\" is not palindrom")
+
+#CODE WHICH IS WRITTEN BY NEETHU MAM
 dict1={"Hello": "world", 1:2}
 dict2= {"zara": "john", "trisha": "sruthi"}
 dict1.update(dict2)
