509060aadfa21b346ae32ceba2c8b5a9cacdf2f0
ANORA\ajays <ajays@anoralabs.com>
2023-03-25 13:56:53 +0530

    CORRECTED FILE FOR FINAL ASSIGNMENT BY AJAY

--- Final Assignment.py
+++ Final Assignment.py
@@ -2,3 +2,12 @@
 
 if __name__ == '__main__':
     print("Demo Program")
+
+# CODE FOR INPUT  STRING IS PALINDROM OR NOT
+
+input_string=input("enter the string: \n")
+reversed_string= input_string[::-1]
+if input_string == reversed_string:
+    print(f"the given string \"{input_string}\" is palindrom")
+else:
+    print(f"the given string \"{input_string}\" is not palindrom")
